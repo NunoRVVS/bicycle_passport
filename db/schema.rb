809575134b70644ab1cd696f8bf04f0ec123563e
Jose Canario <jea.canario@gmail.com>
2024-12-04 11:25:37 +0000

    verified maintenances

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_111051) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_105309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_111051) do
     t.bigint "bicycle_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "verified"
     t.index ["bicycle_id"], name: "index_maintenances_on_bicycle_id"
   end
 
